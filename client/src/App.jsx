c2b265c9864c777faba7a7967d2b79a3c414415b
DanaDietrichHosman <danadh33@gmail.com>
2025-12-29 22:30:26 +0200

    Integrate FileHandler into App layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import FileHandler from './components/FileHandler/FileHandler';
 import { BrowserRouter, Routes, Route, Link } from 'react-router'
 import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <div className={styles.app}>
         <header className={styles.appHeader}>
+          <FileHandler />
           <img src={projectLogo} alt="Logo" className={styles.appLogo} />
           <nav className={styles.appNav}>
             <Link to="/" className={styles.appLink}>Home</Link>
